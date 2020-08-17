a8c88e9175552fcc3cdcc56e335a5016dd4887c1
Yulia Metel <yulichka.metel@gmail.com>
2020-08-17 11:47:27 +0300

    Added admin panel

--- UpdateQABot.py
+++ UpdateQABot.py
@@ -1,9 +1,17 @@
+from bot import event
 from bot.bot import Bot
 from bot.handler import MessageHandler, BotButtonCommandHandler
 import json
 import random
 import Update
 TOKEN = "001.2415440181.2901955096:754789318"
+BOT_UIN = '754789318'
+BOT_NAME = 'UpdateQABot'
+ADMIN_UIN = '740176560'
+ADMIN_NAME = 'Юлия Метель'
+work_chat_id = '681410292@chat.agent'
+testers_free = {}
+black_list = {BOT_UIN: BOT_NAME, ADMIN_UIN: ADMIN_NAME}
 
 bot = Bot(token=TOKEN)
 
@@ -13,9 +21,17 @@ agent_release = Update.Release(Update.CallBacksTexts.agent)
 icq_release = Update.Release(Update.CallBacksTexts.icq)
 myteam_release = Update.Release(Update.CallBacksTexts.myteam)
 
-def responsible():
-    testers = ("741451770", "yulichka_mitryaeva@mail.ru")
-    return random.sample(list(testers), 2)
+
+def all_free_testers(testers_free, black_list, chat_id):
+    testers_all = json.loads(bot.get_chat_members(chat_id=chat_id).text)['members']
+    for tester in testers_all:
+        if tester['userId'] not in testers_free and tester['userId'] not in black_list.keys():
+            tester_info = json.loads(bot.get_chat_info(tester['userId']).text)
+            testers_free.update({tester['userId']: tester_info.get("firstName", '') + ' ' + tester_info.get("lastName", '')})
+    return testers_free
+
+def random_testers(testers):
+    return random.sample(list(testers.keys()), 2)
 
 def get_beta_main_text(agent, icq):
     text = 'Уже протестированы беты:'
@@ -70,6 +86,35 @@ def get_release_main_buttons(agent, myteam, icq):
         button = [{"text": Update.CallBacksTexts.icq.value[1],
                    "callbackData": Update.CallBacksTexts.icq.value[0] + icq_release.type_update.value[0]}]
         mass_buttons.append(button)
+    if len(mass_buttons) == 0:
+        return None
+    else:
+        return mass_buttons
+
+def remove_black_list_buttons(black_list):
+    mass_buttons = []
+    for id in black_list:
+        if id != BOT_UIN and id != ADMIN_UIN:
+            button = [{"text": black_list[id],
+                       "callbackData": id + '_rem'}]
+            mass_buttons.append(button)
+    button = [{"text": 'Назад',
+               "callbackData": 'admin_main'}]
+    mass_buttons.append(button)
+    if len(mass_buttons) == 0:
+        return None
+    else:
+        return mass_buttons
+
+def add_black_list_buttons(testers_free):
+    mass_buttons = []
+    for id in testers_free:
+        button = [{"text": testers_free[id],
+                   "callbackData": id + '_add'}]
+        mass_buttons.append(button)
+    button = [{"text": 'Назад',
+               "callbackData": 'admin_main'}]
+    mass_buttons.append(button)
     if len(mass_buttons) == 0:
         return None
     else:
@@ -329,8 +374,67 @@ def buttons_answer_cb(bot, event):
                                     text="Ты успешно проверил " + platform.value[1] + ', ты молодец 😻',
                                     show_alert=False
                                 )
-
+    elif event.data['callbackData'] == "admin_main":
+        global testers_free
+        testers_free = all_free_testers(testers_free, black_list, work_chat_id)
+        bot.edit_text(
+            chat_id=event.data['message']['chat']['chatId'],
+            text='Что будем делать?',
+            msg_id=event.data["message"]["msgId"],
+            inline_keyboard_markup="{}".format(json.dumps([
+                [{"text": "Удалить из Black-list", "callbackData": "remove_black_list", "style": "attention"}],
+                [{"text": "Добавить в Black-list", "callbackData": "adding_black_list", "style": "primary"}]
+            ])))
+    elif event.data['callbackData'] == "remove_black_list":
+        bot.edit_text(
+            chat_id=event.data['message']['chat']['chatId'],
+            text='Кого уберём из Black list?',
+            msg_id=event.data["message"]["msgId"],
+            inline_keyboard_markup="{}".format(json.dumps(
+                remove_black_list_buttons(black_list)
+            )))
+    elif event.data['callbackData'] == "adding_black_list":
+        bot.edit_text(
+            chat_id=event.data['message']['chat']['chatId'],
+            text='Кого добавим в Black list?',
+            msg_id=event.data["message"]["msgId"],
+            inline_keyboard_markup="{}".format(json.dumps(
+                add_black_list_buttons(testers_free)
+            )))
+    elif '_rem' in event.data['callbackData']:
+        id = event.data['callbackData'][:-4]
+        testers_free.update({id: black_list[id]})
+        del black_list[id]
+        bot.answer_callback_query(
+            query_id=event.data['queryId'],
+            text=f'Пользователь {testers_free[id]} удален из Black list✗',
+            show_alert=False
+        )
+        bot.edit_text(
+            chat_id=event.data['message']['chat']['chatId'],
+            text='Кого уберём из Black list?:',
+            msg_id=event.data["message"]["msgId"],
+            inline_keyboard_markup="{}".format(json.dumps(
+                remove_black_list_buttons(black_list)
+            )))
+    elif '_add' in event.data['callbackData']:
+        id = event.data['callbackData'][:-4]
+        black_list.update({id: testers_free[id]})
+        del testers_free[id]
+        bot.answer_callback_query(
+            query_id=event.data['queryId'],
+            text=f'Пользователь {black_list[id]} добавлен в Black list✓︎',
+            show_alert=False
+        )
+        bot.edit_text(
+            chat_id=event.data['message']['chat']['chatId'],
+            text='Кого добавим в Black list?',
+            msg_id=event.data["message"]["msgId"],
+            inline_keyboard_markup="{}".format(json.dumps(
+                add_black_list_buttons(testers_free)
+            )))
 def message_cb(bot, event):
+    global work_chat_id
     if event.text == '/start' or event.text == '/help':
         bot.send_text(
             chat_id=event.from_chat,
@@ -339,17 +443,36 @@ def message_cb(bot, event):
                  '/startRelease - Начинаем тестирование релиза!\n '
                  '/currentBeta - Тестирование беты уже идет, присоединяйся!\n '
                  '/currentRelease - Тестирование релиза уже идет, присоединяйся!\n'
+                 '/admin - Панель админа(только для администратора бота)\n'
+                 '/set - Чат переехал\n'
                  '/help - Список команд, чтобы не потерять 📒'
+        )
+    elif event.text == '/admin' and event.data['chat']['type'] == 'private' and event.data['chat']['chatId'] == ADMIN_UIN:
+        global testers_free
+        testers_free = all_free_testers(testers_free, black_list, work_chat_id)
+        bot.send_text(
+            chat_id=event.from_chat,
+            text='Что будем делать?',
+            inline_keyboard_markup="{}".format(json.dumps([
+                [{"text": "Удалить из Black list", "callbackData": "remove_black_list", "style": "attention"}],
+                [{"text": "Добавить в Black list", "callbackData": "adding_black_list", "style": "primary"}]
+            ])))
+    elif event.text == '/set' and event.data['chat']['type'] == 'group':
+        work_chat_id = event.data['chat']['chatId']
+        bot.send_text(
+            chat_id=event.from_chat,
+            text='Мы переехали🏕'
         )
     elif event.text == '/startBeta':
         global agent_beta
         agent_beta = Update.Beta(Update.CallBacksTexts.agent)
         global icq_beta
         icq_beta = Update.Beta(Update.CallBacksTexts.icq)
-        test_user = responsible()
+        work_chat_id = event.data['chat']['chatId']
+        testers_do = random_testers(all_free_testers(testers_free, black_list, event.data['chat']['chatId']))
         bot.send_text(
             chat_id=event.from_chat,
-            text=f'@[{test_user[0]}] и @[{test_user[1]}] \n Начинаем тестирование беты! ',
+            text=f'@[{testers_do[0]}] и @[{testers_do[1]}] \n Начинаем тестирование беты! ',
             inline_keyboard_markup="{}".format(json.dumps(
                 get_beta_main_buttons(agent_beta, icq_beta)
             )))
@@ -360,10 +483,11 @@ def message_cb(bot, event):
         icq_release = Update.Release(Update.CallBacksTexts.icq)
         global myteam_release
         myteam_release = Update.Release(Update.CallBacksTexts.myteam)
-        test_user = responsible()
+        work_chat_id = event.data['chat']['chatId']
+        testers_do = random_testers(all_free_testers(testers_free, black_list, event.data['chat']['chatId']))
         bot.send_text(
             chat_id=event.from_chat,
-            text=f'@[{test_user[0]}] и @[{test_user[1]}] \n Начинаем тестирование релиза! ',
+            text=f'@[{testers_do[0]}] и @[{testers_do[1]}] \n Начинаем тестирование релиза! ',
             inline_keyboard_markup="{}".format(json.dumps(
                 get_release_main_buttons(agent_release, myteam_release, icq_release)
             )))
